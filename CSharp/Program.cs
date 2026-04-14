a2248d6799bccd4c1e0d5e5a6283a1159f3b0028
Митя <elita700@gmail.com>
2026-04-14 11:06:32 +0600

    CSharp files

---
+++ CSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;  // для списка истории
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+                    Console.WriteLine("Введите число");
+            Console.ReadLine();
+            int.Parse(Console.ReadLine());
+                
+            
+        }
+    }
+}
